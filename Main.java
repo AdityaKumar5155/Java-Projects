5feeb1a9e64774133a0a32bc54ca3b89d1088ff9
Aditya Kumar <adityakumar5155@gmail.com>
2022-12-18 22:18:35 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,34 +2,34 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        int n,k;
+        int n,k;                                  
         String n_str;
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);  
         n = sc.nextInt();
         k = sc.nextInt();
-        n_str = Integer.toString(n);
-        Main obj = new Main();
-        String answer_str = obj.rotate(n_str,k);
-        int answer = Integer.parseInt(answer_str);
+        n_str = Integer.toString(n);                     //Converting Int to String type for further operations
+        Main obj = new Main();                           
+        String answer_str = obj.rotate(n_str,k);         //rotated string
+        int answer = Integer.parseInt(answer_str);       //rotated Integer
         System.out.println(answer);
         sc.close();
     }
     String rotate(String str , int k){
         String str1="";
         String str2="";
-        int len = str.length();
+        int len = str.length();                          //Getting length of string
         if (k>=0){
-            k = k%len;
-            str1 = str.substring(len-k , len);
-            str2 = str.substring(0,len-k);
+            k = k%len;                                   //Taking modulus of k with length of string to avoid index OutOfRange Error
+            str1 = str.substring(len-k , len);           //Getting last k letters
+            str2 = str.substring(0,len-k);               //Getting remaining letters
         }
         else{
             k = k*(-1);
             k = k%len;
-            str1 = str.substring(k, len);
-            str2 = str.substring(0, k);
+            str1 = str.substring(k, len);                //Getting all letters except first k letters
+            str2 = str.substring(0, k);                  //Getting first k letters
         }
-        String answer = str1+str2;
+        String answer = str1+str2;                       //Adding the both pieces of string
         return answer;
     }
 }
